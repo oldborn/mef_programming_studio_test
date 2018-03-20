6fecbe15d95c3201a55e3bf82e534b9982f8c7c3
rocko <s.safak.topcu@gmail.com>
2018-03-20 10:25:30 +0300

    Added: an integer

--- test_fx/src/test_fx/Test_fx.java
+++ test_fx/src/test_fx/Test_fx.java
@@ -20,7 +20,7 @@ public class Test_fx extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        int a = 4;
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
